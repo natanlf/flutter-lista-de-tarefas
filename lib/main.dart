21eaae95d7cd272a27b929ac6916e398d11c730f
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-05-25 21:56:07 -0300

    Snackbar e Ordenacao
    Desfazendo ação e ordenando itens

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,9 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     //ciclo de vida da inicialização
@@ -41,6 +44,22 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+   setState(() {
+     _toDoList.sort((a, b) {
+       if(a["ok"] && !b["ok"]) return 1;
+       else if(!a["ok"] && b["ok"]) return -1;
+       else return 0;
+     });
+
+     _saveData();
+   });
+
+   return null;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,16 +101,18 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _toDoList.length,
-                  itemBuilder: buildItem))
+              child: RefreshIndicator(
+                  onRefresh: _refresh,
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 10.0),
+                      itemCount: _toDoList.length,
+                      itemBuilder: buildItem)))
         ],
       ),
     );
   }
 
-  Widget buildItem(context, index) {
+  Widget buildItem(BuildContext context, int index) {
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
@@ -117,6 +138,30 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        _lastRemoved = Map.from(_toDoList[index]); //copia o que vamos remover
+        _lastRemovedPos = index;
+        _toDoList.removeAt(index);
+
+        _saveData();
+
+        final snack = SnackBar(
+          content: Text("Tarefa \"${_lastRemoved["title"]}\" remivida!"),
+          action: SnackBarAction(
+            label: "Desfazer",
+            onPressed: () {
+              setState(() {
+                _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+            },
+          ),
+          duration: Duration(seconds: 2),
+        );
+
+        Scaffold.of(context).removeCurrentSnackBar();    // ADICIONE ESTE COMANDO
+        Scaffold.of(context).showSnackBar(snack);
+      },
     );
   }
 
